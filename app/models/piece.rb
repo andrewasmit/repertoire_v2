68228706903aad0f9c4826b7ad7c7e4ee5e4df31
andrew <andrewasmit@hotmail.com>
2023-08-29 12:47:33 -0500

    added test validations for Piece model

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,4 +2,6 @@ class Piece < ApplicationRecord
   has_many :notes
   has_many :performances
   has_many :concerts, through: :performances
+
+  validates_presence_of :title, :composer, :number_of_players, :difficulty, :genre
 end
--- test/models/piece_test.rb
+++ test/models/piece_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
+require 'faker'
 
 class PieceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Add Pieces w/ correct validations" do
+    piece = Piece.new
+    piece.composer = Faker::Name.name
+    piece.title = Faker::Quote.yoda
+    piece.difficulty = Faker::Number.between(from: 1, to: 5)
+    piece.number_of_players = Faker::Number.between(from: 1, to: 12)
+    piece.genre = Faker::Music.genre
+    byebug
+
+    assert piece.save,  "Piece should save with correct validations"
+  end
 end
