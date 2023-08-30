0084e4fd0823f19a2661260b64a3d1a16547a1d1
andrew <andrewasmit@hotmail.com>
2023-08-30 15:46:42 -0500

    add presence validations to all models and init tests for ensembles

--- app/models/ensemble.rb
+++ app/models/ensemble.rb
@@ -2,4 +2,6 @@ class Ensemble < ApplicationRecord
   has_many :performances
   has_many :concerts, through: :performances
   belongs_to :organization
+
+  validates_presence_of :organization_id, :name, :grade_level
 end
--- test/models/ensemble_test.rb
+++ test/models/ensemble_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
+require 'faker'
 
 class EnsembleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'Ens should save with correct validations.' do
+    organization = Organization.create(name: 'Testing Organization')
+
+    ens = Ensemble.new
+    ens.organization_id = organization.id
+    ens.name = Faker::Lorem.characters(number: 5, min_alpha: 3, min_numeric: 1)
+    ens.grade_level = Faker::Lorem.characters(number: 5, min_alpha: 3, min_numeric: 1)
+
+    
+    assert ens.save, 'Ens should save with correct validations.'
+  end
+
 end
