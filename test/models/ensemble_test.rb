316f47c6d203f5f71f08d5fcaa8368cf88aeb53e
andrew <andrewasmit@hotmail.com>
2023-08-31 06:58:33 -0500

    happy and sad paths for note tests and general cleanup

--- test/models/ensemble_test.rb
+++ test/models/ensemble_test.rb
@@ -2,15 +2,15 @@ require "test_helper"
 
 class EnsembleTest < ActiveSupport::TestCase
 
-  # test 'Ens should save with correct validations.' do
-  #   organization = Organization.create(name: 'Testing Organization')
+  test 'Ens should save with correct validations.' do
+    organization = Organization.create(name: 'Testing Organization')
 
-  #   ens = Ensemble.new
-  #   ens.organization_id = organization.id
-  #   ens.name = Faker::Educator.primary_school
-  #   ens.grade_level = Faker::Lorem.characters(number: 1, min_alpha: 0, min_numeric: 1)
+    ens = Ensemble.new
+    ens.organization_id = organization.id
+    ens.name = Faker::Educator.primary_school
+    ens.grade_level = Faker::Lorem.characters(number: 1, min_alpha: 0, min_numeric: 1)
     
-  #   assert ens.save, 'Ens should save with correct validations.'
-  # end
+    assert ens.save, 'Ens should save with correct validations.'
+  end
 
 end
--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -3,35 +3,35 @@ require 'faker'
 
 class NoteTest < ActiveSupport::TestCase
 
-  # test "Add Notes w/ correct validations" do
+  test "Add Notes w/ correct validations" do
     # Variable Definitions
-    # piece = Piece.create(
-    #   composer: Faker::Name.name, 
-    #   title: Faker::Quote.yoda,
-    #   difficulty: Faker::Number.between(from: 1, to: 5),
-    #   number_of_players: Faker::Number.between(from: 1, to: 5),
-    #   genre: Faker::Music.genre
-    # )
+    piece = Piece.create(
+      composer: Faker::Name.name, 
+      title: Faker::Quote.yoda,
+      difficulty: Faker::Number.between(from: 1, to: 5),
+      number_of_players: Faker::Number.between(from: 1, to: 5),
+      genre: Faker::Music.genre
+    )
   
-    # organization = Organization.create(name: 'Testing Organization')
-    # password = Faker::Lorem.characters(number: 10, min_alpha: 4, min_numeric: 1)
-    # username = Faker::Lorem.characters(number: 8, min_alpha: 1, min_numeric: 1)
+    organization = Organization.create(name: 'Testing Organization')
+    password = Faker::Lorem.characters(number: 10, min_alpha: 4, min_numeric: 1)
+    username = Faker::Lorem.characters(number: 8, min_alpha: 1, min_numeric: 1)
   
-    # user = User.create(
-    #   username: username,
-    #   password: password,
-    #   password_confirmation: password,
-    #   email_address: username + '@gmail.com',
-    #   organization_id: organization.id
-    # )
-
+    user = User.create(
+      username: username,
+      password: password,
+      password_confirmation: password,
+      email_address: username + '@gmail.com',
+      organization_id: organization.id
+    )
 
     # Test case definitions
-    # note = Note.new
-    # note.note = Faker::Quote.yoda
-    # note.user_id = user.id
-    # note.piece_id = piece.id
+    note = Note.new
+    note.note = Faker::Quote.yoda
+    note.user_id = user.id
+    note.piece_id = piece.id
 
-    # assert note.save,  "Note should save with correct validations"
-  # end
+    assert note.save,  "Note should save with correct validations"
+  end
+  
 end
