8b0bddc3d4f926368f4e9ce104e6aab17f88a784
andrew <andrewasmit@hotmail.com>
2023-08-31 06:51:06 -0500

    chore: happy/sad path testing for all User tests and comment cleanup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,5 @@
 require 'test_helper'
 require 'faker'
-require 'byebug'
 
 class UserTest < ActiveSupport::TestCase
   
@@ -15,41 +14,38 @@ class UserTest < ActiveSupport::TestCase
     user.password_confirmation = password
     user.email_address = username + '@gmail.com'
     user.organization_id = organization.id
-    # user.is_admin = false
-    byebug
+    
     assert user.save, 'User should save if validations are correct.'
   end
 
-  # test 'User should not save if username length is less than 6.' do
-  #   organization = Organization.create(name: 'Testing Organization')
-  #   password = Faker::Lorem.characters(number: 10, min_alpha: 4, min_numeric: 1)
-  #   username = Faker::Lorem.characters(number: 5, min_alpha: 1, min_numeric: 1)
+  test 'User should not save if username length is less than 6.' do
+    organization = Organization.create(name: 'Testing Organization')
+    password = Faker::Lorem.characters(number: 10, min_alpha: 4, min_numeric: 1)
+    username = Faker::Lorem.characters(number: 5, min_alpha: 1, min_numeric: 1)
 
-  #   user = User.new
-  #   user.username = username
-  #   user.password = password
-  #   user.password_confirmation = password
-  #   user.email_address = username + '@gmail.com'
-  #   user.organization_id = organization.id
+    user = User.new
+    user.username = username
+    user.password = password
+    user.password_confirmation = password
+    user.email_address = username + '@gmail.com'
+    user.organization_id = organization.id
     
-  #   assert_not user.save, 'User should not save if username length is less than 6.'
-  # end
+    assert_not user.save, 'User should not save if username length is less than 6.'
+  end
 
-  # test 'User should not save if password length is less than 6.' do
-  #   organization = Organization.create(name: 'Testing Organization')
-  #   password = Faker::Lorem.characters(number: 3, min_alpha: 1, min_numeric: 1)
-  #   username = Faker::Lorem.characters(number: 8, min_alpha: 1, min_numeric: 1)
+  test 'User should not save if password length is less than 6.' do
+    organization = Organization.create(name: 'Testing Organization')
+    password = Faker::Lorem.characters(number: 3, min_alpha: 1, min_numeric: 1)
+    username = Faker::Lorem.characters(number: 8, min_alpha: 1, min_numeric: 1)
 
-  #   user = User.new
-  #   user.username = username
-  #   user.password = password
-  #   user.password_confirmation = password
-  #   user.email_address = username + '@gmail.com'
-  #   user.organization_id = organization.id
+    user = User.new
+    user.username = username
+    user.password = password
+    user.password_confirmation = password
+    user.email_address = username + '@gmail.com'
+    user.organization_id = organization.id
     
-  #   assert_not user.save, 'User should not save if password length is less than 6.'
-  # end
+    assert_not user.save, 'User should not save if password length is less than 6.'
+  end
 
-  
-  
 end
