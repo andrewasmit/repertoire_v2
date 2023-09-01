d7e89dd965d6dffdec526452bbc10c3745392f5d
andrew <andrewasmit@hotmail.com>
2023-09-01 10:56:42 -0500

    chore: validate presence of name to new orgs

--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -3,8 +3,14 @@ require "test_helper"
 class OrganizationTest < ActiveSupport::TestCase
   test "Organizations should have a UUID auto-generated when initialized" do
     org = Organization.create(name:'Test Org')
-    byebug
     
     assert org.uuid, "Organizations should have a UUID auto-generated when initialized"
   end
+
+  test "Organizations should not be able to save to db without a name" do
+    org = Organization.new
+    
+    assert_not org.save, "Organizations should not be able to save to db without a name"
+  end
+
 end
