94c499b2a4a21c108a71e5f2d9038bce585e774b
andrew <andrewasmit@hotmail.com>
2023-09-01 10:45:17 -0500

    feat: auto geneate uuid on init of organizations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,15 @@
 class Organization < ApplicationRecord
+  before_create :add_uuid
+
   has_many :users
   has_many :ensembles
   has_many :concerts
 
   validates_presence_of :name
+
+  private
+  def add_uuid
+    self.uuid = SecureRandom.uuid
+  end
+  
 end
