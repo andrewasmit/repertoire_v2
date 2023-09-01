c31f4f35ebab2b2a037706c50acb93ce7eb6621c
andrew <andrewasmit@hotmail.com>
2023-09-01 11:40:25 -0500

    feat:seed db with basic org and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Seeding db 🌱'
+
+org = Organization.create(name: 'Smit School of Percussion')
+me = User.create(
+  username: 'andrewasmit',
+  password: '123456',
+  password_confirmation: '123456',
+  email_address: 'fake@email.com',
+  organization_id: org.id,
+  is_admin: true
+)
+
+puts 'Finished seeding db ✅'
