02f3d3b7e6dc17d48d30baff27d0f265d7c4fcca
andrew <andrewasmit@hotmail.com>
2023-09-03 15:37:29 -0500

    feat: added create and show routes/methods for organizations

--- app/controllers/organization_controller.rb
+++ app/controllers/organization_controller.rb
@@ -1,2 +1,23 @@
 class OrganizationController < ApplicationController
+
+  def create
+    org = Organization.create!(organization_params)
+    render json: org, status: :created
+  end
+
+  def show
+    user = User.find(session[:user_id])
+    org = Organization.find(params[:id])
+    if user.organization_id == org.id
+      render json: org, status: :ok
+    else
+      render json: { error: 'You are not authorized to view or make edits to organizaitons you are not associated with.' }
+    end
+  end
+
+  private
+  def organization_params
+    params.permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:create, :show, :update, :destroy]
+  resources :organization, only: [:create, :show, :update, :destroy]
 
   post '/login', to: 'session#create'
   delete '/logout', to: 'session#destroy'
