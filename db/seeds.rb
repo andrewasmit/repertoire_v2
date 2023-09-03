bf7247ff2ac46b94fb44c75888535aff16c34607
andrew <andrewasmit@hotmail.com>
2023-09-03 13:34:41 -0500

    feat: updated seeds to have an instance and relationship oif each model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 puts 'Seeding db 🌱'
 
 org = Organization.create(name: 'Smit School of Percussion')
@@ -9,6 +10,38 @@ me = User.create(
   organization_id: org.id,
   is_admin: true
 )
-byebug
+
+june_bug = Piece.create(
+  title: 'June Bug',
+  composer: 'Andrew Smit',
+  number_of_players: 1,
+  genre: 'solo',
+  difficulty: 2,
+  reference_recording: 'https://www.youtube.com/watch?v=kwUETmmisKY'
+)
+
+note = Note.create(
+  piece_id: june_bug.id,
+  user_id: me.id,
+  note: Faker::Lorem.paragraph
+)
+
+concert = Concert.create(
+  title: Faker::Show.adult_musical,
+  year: 2023,
+  organization_id: org.id
+)
+
+ensemble = Ensemble.create(
+  organization_id: org.id,
+  name: 'Beginner Percussion',
+  grade_level: 6
+)
+
+performance = Performance.create(
+  piece_id: june_bug.id,
+  ensemble_id: ensemble.id,
+  concert_id: concert.id
+)
 
 puts 'Finished seeding db ✅'
