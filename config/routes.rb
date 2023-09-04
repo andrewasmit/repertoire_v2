0db74e4074c21db5355d29d10009f32f3dabb33e
andrew <andrewasmit@hotmail.com>
2023-09-04 07:31:29 -0500

    refactor: renamed sessions controllers to match naming conventions

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :show, :update, :destroy]
   resources :organizations, only: [:create, :show, :update, :destroy]
 
-  post '/signin', to: 'session#create'
-  delete '/signout', to: 'session#destroy'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
   post '/signup', to: 'user#create'
   
 end
