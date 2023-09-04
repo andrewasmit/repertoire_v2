7296f4902d82be90e6609b55e9ee91e767db2e89
andrew <andrewasmit@hotmail.com>
2023-09-03 21:13:11 -0500

    feat: full crud routes and methods complete with error handling for organizations

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,4 +1,5 @@
 class OrganizationsController < ApplicationController
+  before_action :authorize_user_crud, only: [:show, :update, :delete]
 
   def create
     org = Organization.create!(organization_params)
@@ -6,18 +7,31 @@ class OrganizationsController < ApplicationController
   end
 
   def show
-    user = User.find(session[:user_id])
     org = Organization.find(params[:id])
-    if user.organization_id == org.id
-      render json: org, status: :ok
-    else
-      render json: { error: 'You are not authorized to view or make edits to organizaitons you are not associated with.' }
-    end
+    render json: org, status: :ok 
+  end
+
+  def update
+    org = Organization.find(params[:id])
+    org.update!(organization_params)
+    render json: org, status: :ok
+  end
+
+  def destroy
+    org = Organization.find(params[:id])
+    org.destroy
+    head :no_content
   end
 
   private
   def organization_params
-    params.permit(:name)
+    params.permit(:name, :id)
+  end
+
+  def authorize_user_crud
+    org = Organization.find(params[:id])
+    user = User.find(session[:user_id])
+    return render json: { error: 'You are not authorized to view or make edits to organizaitons you are not associated with or an Administrator of.' }, status: :unauthorized unless user.organization_id == org.id && user.is_admin
   end
 
 end
