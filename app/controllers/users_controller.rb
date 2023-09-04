7c82145e04059230b004a4adce1daa715e0fe070
andrew <andrewasmit@hotmail.com>
2023-09-04 07:25:05 -0500

    refactor: added before actions to locate and authenticate users before any CRUD actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+  attr_accessor :user
   skip_before_action :authorize, only: [:create]
+  before_action :find_and_authenticate_user, only: [:show, :update, :destroy]
 
   def create
     user = User.create!(user_params)
@@ -8,24 +10,17 @@ class UsersController < ApplicationController
   end
 
   def show
-    user = User.find(session[:user_id])
-    if params[:id].to_i == user.id 
-      render json: user, status: :ok
-    else
-      render json: { error: "You are only authorized to view the user who is signed in." }, status: :unauthorized
-    end
+    render json: @user, status: :ok
   end
 
   def update
-    user = User.find(session[:user_id])
-    user.update(user_params)
+    @user.update(user_params)
     render json: user, status: :ok
   end
 
   def destroy
-      user = User.find(session[:user_id])
-      user.destroy
-      head :no_content
+    @user.destroy
+    head :no_content
   end
 
   private
@@ -38,6 +33,11 @@ class UsersController < ApplicationController
       :organization_id,
       :id
     )
+  end
+
+  def find_and_authenticate_user
+    @user = User.find(session[:user_id])
+    return render json: { error: 'You are only authorized to view or make changes to the user who is signed in.' }, status: :unauthorized unless @user.id == session[:user_id] && params[:id].to_i == session[:user_id]
   end
 
 end
