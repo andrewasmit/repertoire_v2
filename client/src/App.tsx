305d15b43fba2c3e8cc5f921d2395b59a3f48e5d
andrew <andrewasmit@hotmail.com>
2023-09-10 07:49:02 -0500

    chore: remove console.logs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,12 +30,12 @@ function App() {
     isLoading: fetchedSolosLoading, 
   } = useFetch(`${import.meta.env.VITE_PERC_LIBRARY_BASE}solos?`, 'fetchedPercussionSolos');
 
-  // console.log("useQuery: ENSEMBLE DATA", fetchedEnsemblesData);
-  // console.log("useQuery: SOLO DATA", fetchedSolosData);
+  console.log("useQuery: ENSEMBLE DATA", fetchedEnsemblesData);
+  console.log("useQuery: SOLO DATA", fetchedSolosData);
 
   const isLoading = fetchedEnsemblesLoading || fetchedSolosLoading;
  
-  // console.log("Currently signed in: ", user);
+  console.log("Currently signed in: ", user);
   useEffect(()=>{
     fetch("/api/me")
     .then((res) => {
@@ -45,7 +45,7 @@ function App() {
           navigate('/home');
         });
       } else {
-        navigate('/home');
+        navigate('/signin');
         console.log("No Session Detected. Navigating to Signin page.");
       }
     })
--- client/src/hooks/userSignOut.tsx
+++ client/src/hooks/userSignOut.tsx
@@ -3,6 +3,5 @@ export const userSignOut = () => {
   fetch("/api/signout", {
     method: "DELETE",
   })
-    .then((data) => console.log("DATA: ", data))
     .catch((err) => console.log("ERROR: ", err));
 };
