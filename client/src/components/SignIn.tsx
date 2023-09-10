6eeecfd4f956845dafa462950601da438c693ed4
andrew <andrewasmit@hotmail.com>
2023-09-09 20:30:38 -0500

    feat: formik addition with boiler plate sign in

--- client/src/components/SignIn.tsx
+++ client/src/components/SignIn.tsx
@@ -1,15 +1,57 @@
+// External Dependencies
+import { Formik, Field, Form, FormikHelpers } from 'formik';
 // import React from 'react'
 
-import Loading from "./shared/Loading"
+// Local Dependencies
+// import Loading from "./shared/Loading"
 
 function SignIn() {
+
+interface Values {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
 
   return (
     <div>
       <h1>This is the Sign In page.</h1>
-      {/* <Loading /> */}
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+        }}
+        onSubmit={(
+          values: Values,
+          { setSubmitting }: FormikHelpers<Values>
+        ) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 500);
+        }}
+      >
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field id="firstName" name="firstName" placeholder="John" />
+
+          <label htmlFor="lastName">Last Name</label>
+          <Field id="lastName" name="lastName" placeholder="Doe" />
+
+          <label htmlFor="email">Email</label>
+          <Field
+            id="email"
+            name="email"
+            placeholder="john@acme.com"
+            type="email"
+          />
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
     </div>
   )
 }
 
-export default SignIn
+export default SignIn;
