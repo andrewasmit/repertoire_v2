de4f49c198c4a2f3e2d310cc2e95c38529b87a0f
andrew <andrewasmit@hotmail.com>
2023-09-23 07:09:51 -0500

    feat: WIP - creating hook for submitting form for new ens

---
+++ client/src/hooks/api/ensembleHooks.ts
@@ -0,0 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
+import { Ensemble } from "../../redux/organizationSlice";
+// import { useCallback } from "react";
+// import { FetchedPiece } from "../../redux/fetchedLibrarySlice";
+
+type EnsembleData = {
+  name: string;
+  grade_level: string;
+};
+
+export const postNewEnsemble = async (
+  newEnsData: EnsembleData
+): Promise<Ensemble> => {
+  const res = await fetch("api/ensembles", {
+    method: "POST",
+    body: JSON.stringify(newEnsData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await res.json();
+
+  if (res.status === 200) {
+    return data;
+  } else throw new Error(data.message);
+};
+
+export const useFetchExternalLibrary = (url: string, key: string) => {
+  const queryFn = useCallback(() => {
+    return fetchData(url);
+  }, [url]);
+
+  return useQuery({
+    queryKey: [`${key}`],
+    queryFn,
+    retry: 1,
+  });
+};
