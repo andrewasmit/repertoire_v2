e843f5387707093f64b966917cdfb90558e7726f
andrew <andrewasmit@hotmail.com>
2023-10-06 15:12:37 -0500

    feat: added MUI DataTable for library page

---
+++ client/src/pages/Library/hooks.ts
@@ -0,0 +1,58 @@
+// External Dependencies
+import { GridColDef } from "@mui/x-data-grid";
+
+// {
+// composer: "Someone",
+// difficulty: 5,
+// genre: "mallet",
+// number_of_players: 4,
+// reference_recording: "click here",
+// title: "Title of the song",
+// }
+
+export const useColumns = (): GridColDef[] => {
+  return [
+    {
+      field: "composer",
+      headerName: "Composer",
+      width: 160,
+    },
+    {
+      field: "title",
+      headerName: "Title",
+      width: 180,
+    },
+    {
+      field: "genre",
+      headerName: "Genre",
+      width: 100,
+    },
+    {
+      field: "number_of_players",
+      headerName: "# of Players",
+      type: "number",
+      width: 105,
+    },
+    {
+      field: "difficulty",
+      headerName: "Difficulty",
+      type: "number",
+      width: 80,
+    },
+    {
+      field: "reference_recording",
+      headerName: "Reference Recording",
+      // type: "number",
+      width: 250,
+    },
+    // {
+    // field: "difficulty",
+    // headerName: "Difficulty",
+    // description: "This column has a value getter and is not sortable.",
+    // sortable: false,
+    //   width: 160,
+    //   valueGetter: (params: GridValueGetterParams) =>
+    //     `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    // },
+  ];
+};
--- client/src/pages/Library/index.tsx
+++ client/src/pages/Library/index.tsx
@@ -1,16 +1,46 @@
 // External Dependencies
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
 
 // Internal Dependencies
+import { useAppSelector } from '../../redux/hooks';
 
 // Local Dependencies
+import { useColumns } from './hooks';
 
 
 function Library() {
+
+  const { library } = useAppSelector(state=>state.organization);
+  
+  const columns = useColumns();
+
   return (
     <div id='library-page'>
-      <h1>This is the Library homepage</h1>
+      <Box sx={{ height: 600, width: '100%' }}>
+
+        <h1>This is the Library Page</h1>
+
+        { library !== null ? 
+        <DataGrid
+          rows={library}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 10,
+              },
+            },
+          }}
+          pageSizeOptions={[10]}
+          // checkboxSelection
+          disableRowSelectionOnClick
+        /> :
+        <h2>There are currently no pieces in your library</h2>
+        }
+      </Box>
     </div>
-  )
+  );
 }
 
 export default Library;
