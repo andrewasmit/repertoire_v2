ada89e1a167f35cc01cdfbefea0da9a5e8b24386
andrew <andrewasmit@hotmail.com>
2023-11-04 19:40:28 -0500

    refactor: theme provided and new color palette added

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,6 +7,8 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import { ThemeProvider, createTheme } from '@mui/material';
+
 
 // Local Dependencies
 import './index.css'
@@ -21,12 +23,37 @@ export const queryClient = new QueryClient({
   },
 });
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#143B38',
+    },
+    secondary: {
+      main: '#E4CEC5'
+    },
+    info: {
+      main: '#D7EA21'
+    },
+    error: {
+      main: '#f44336'
+    },
+    success: {
+      main: '#66bb6a'
+    },
+    warning: {
+      main: '#D7EA21'
+    }
+  }
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
-        <Provider store={store}>
-          <App />
-        </Provider>
+      <ThemeProvider theme={theme} >
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </ThemeProvider>
       </QueryClientProvider>
   </BrowserRouter>,
 );
