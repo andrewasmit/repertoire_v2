4154ea4f86f21839f3ff830bbcff41ca1fe7d6e2
andrew <andrewasmit@hotmail.com>
2023-11-04 20:43:06 -0500

    refactor: tab navbar up

--- client/src/components/shared/NavBar.tsx
+++ client/src/components/shared/NavBar.tsx
@@ -1,6 +1,7 @@
 // External Depencies
-import { useCallback } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useCallback, useState } from "react";
+import { useNavigate, useParams } from 'react-router-dom';
+import { Box, Button, ButtonGroup, Tab, Tabs, Typography } from "@mui/material";
 
 // Local Dependencies
 import MyAccountBtn from "./MyAccountBtn";
@@ -11,6 +12,32 @@ import logo from '/logo-no-background.svg'
 function NavBar() {
 
   const navigate = useNavigate();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+    handleNavigate(newValue);
+  };
+
+  const handleNavigate = useCallback((value: number): void=>{
+    switch (value){
+      case 0:
+        navigate('/home');
+        break;
+      case 1:
+        navigate('/ensembles');
+        break;
+      case 2:
+        navigate('/concerts');
+        break;
+      case 3:
+        navigate('/library');
+        break;
+      case 4:
+        navigate('/browse');
+        break;
+    }
+  },[]);
 
   const handleNavToDash = useCallback((): void=>{
     navigate('/home');
@@ -31,13 +58,20 @@ function NavBar() {
   const handleNavToBrowse = useCallback((): void=>{
     navigate('/browse');
   }, []);
+  
+  function a11yProps(index: number) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
 
   return (
     <div className="navbar">
       <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
         <img src={logo} className="logo" alt="Main logo" />
       </a>
-      <ul>
+      {/* <ul>
         <li>
           <button onClick={handleNavToDash} >Dashboard</button>
         </li>
@@ -53,7 +87,25 @@ function NavBar() {
         <li>
           <button onClick={handleNavToBrowse} >Find New Music</button>
         </li>
-      </ul>
+      </ul> */}
+
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs 
+          value={value} 
+          onChange={handleChange} 
+          aria-label="basic tabs example"
+          textColor="inherit"
+          color='secondary'
+          indicatorColor="secondary"
+        >
+          <Tab label="Dashboard" {...a11yProps(0)} />
+          <Tab label="Ensembles" {...a11yProps(1)} />
+          <Tab label="Concerts" {...a11yProps(2)} />
+          <Tab label="Library" {...a11yProps(3)} />
+          <Tab label="Find New Music" {...a11yProps(4)} />
+        </Tabs>
+      </Box>
+
       <MyAccountBtn />
     </div>
   )
