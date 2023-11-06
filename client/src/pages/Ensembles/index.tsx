298221356b54afd8f81a6af4f8916a8a7334c031
andrew <andrewasmit@hotmail.com>
2023-11-05 21:08:46 -0600

    feat: header for ensemble show page updated with MUI boxes and theme styling

--- client/src/pages/Ensembles/index.tsx
+++ client/src/pages/Ensembles/index.tsx
@@ -34,28 +34,42 @@ function Ensembles() {
   const btnStyles = {
     marginTop: 3,
     ':hover':{
-      color: theme.palette.secondary.main,
-      bgcolor: theme.palette.primary.main
+      color: theme.palette.primary.main,
+      bgcolor: theme.palette.info.main
     }
   };
 
 
   return (
     <Box className="ens-page" sx={{ background: '#fff' }}>
-      <Typography variant="h3" sx={{ marginTop: 0, padding: 1 }}>{organization?.name}</Typography>
+      <Box 
+        className="ens-header" 
+        sx={{ 
+          background: theme.palette.primary.main, 
+          color: theme.palette.secondary.main,
+          padding: 5,
+          }} 
+        >
+        <Typography 
+          variant="h3" 
+          sx={{ marginTop: 0, padding: 1, color: theme.palette.info.main }}
+        >
+          {organization?.name}
+        </Typography>
 
-      <Typography variant="h5">
-        Here are the ensembles currently in your organization
-      </Typography>
+        <Typography variant="h5">
+          Here are the ensembles currently in your organization
+        </Typography>
 
-      <Button 
-        onClick={handleAddEnsClick} 
-        variant="contained" 
-        color="secondary" 
-        sx={btnStyles}
-      >
-        Add New Ensemble
-      </Button>
+        <Button 
+          onClick={handleAddEnsClick} 
+          variant="contained" 
+          color="secondary" 
+          sx={btnStyles}
+        >
+          Add New Ensemble
+        </Button>
+      </Box>
 
       <div id="ens-card-container">
         {ensembleCards}
