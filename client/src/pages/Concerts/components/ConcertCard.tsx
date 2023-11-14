055ffb48162a75ffbe67ebb8712c0ff27bee89f3
andrew <andrewasmit@hotmail.com>
2023-11-14 14:06:25 -0600

    feat: massive styling updates to concertcards

--- client/src/pages/Concerts/components/ConcertCard.tsx
+++ client/src/pages/Concerts/components/ConcertCard.tsx
@@ -1,13 +1,11 @@
 // External Dependencies
 import { useNavigate } from "react-router-dom";
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
+import { Box, List, ListItem, Typography, useTheme } from "@mui/material";
 
 // Internal Dependencies
 import { PerformedPiece } from "../../../redux/organizationSlice"
-
-// Local Dependencies
-import '../concerts.css'
-import { List, ListItem, Typography } from "@mui/material";
+import { useAppSelector } from "../../../redux/hooks";
 
 type ConcertCardParams = {
   year: number | string;
@@ -19,40 +17,71 @@ type ConcertCardParams = {
 function ConcertCard({ year, id, name, program }: ConcertCardParams) {
 
   const navigate = useNavigate();
+  const { ensembles } = useAppSelector(state=>state.organization);
+  const theme = useTheme();
 
   const handleNavToShowPage = useCallback((): void=>{
     navigate(`/concerts/${id}`);
   }, []);
 
-  console.log("PROGRAM: ", program)
-  
-  const performancesToDisplay = program?.map(performance=>{
+  const performingEnsembles = useMemo(()=>{
+    const allPerformanceEnsembleIds = program?.map(performance=>{
+      return performance.ensemble_id;
+    });
+
+    const ensembleIds = [...new Set(allPerformanceEnsembleIds)];
+
+    return ensembleIds.map(ensId=>{
+      return ensembles?.find(ens=>ens.id === ensId);
+    });
+
+  }, [program, ensembles]);
+
+  const performingEnsemblesToDisplay = performingEnsembles?.map(ens=>{
     return <ListItem 
-              key ={performance.performance_id}
+              key ={ens?.id}
               sx={{ 
                 display: 'flex', 
                 alignItems: 'center', 
                 justifyContent: 'center',
+                color: theme.palette.secondary.main,
+                background: theme.palette.primary.main,
+                opacity: 0.7,
                 ':nth-child(odd)':{
-                  background: '#efefef'
+                  opacity: 0.8
                 }
               }}
             >
-              <Typography variant="body2"><i>{performance.piece}</i></Typography>
+              <Typography variant="body2"><i>{ens?.name}</i></Typography>
             </ListItem>
   });
 
+  const boxStyles = { 
+    maxWidth: '300px', 
+    textAlign: 'center', 
+    border: `1px solid ${theme.palette.info.main}`, 
+    ':hover':{
+      opacity: 0.85,
+      cursor: 'pointer',
+      border: `${theme.palette.secondary.main} 1px solid`
+    }
+  };
+
+  const headerStyles = {
+    background: theme.palette.info.main,
+    opacity: 0.7,
+    color: theme.palette.primary.main
+  }
 
   return (
-    <div id={`concert-${id}`} className="concert-card" onClick={handleNavToShowPage}>
-      <div className="concert-card-wrapper">
-        <Typography variant="h5" color="secondary" >{name}</Typography>
-        <Typography variant="h6" color="secondary">{year}</Typography>
-        <List>
-          {performancesToDisplay}
-        </List>
-      </div>
-    </div>
+    <Box id={`concert-${id}`} sx={boxStyles} onClick={handleNavToShowPage}>
+      <Typography variant="h5" sx={headerStyles} >{name}</Typography>
+      <Typography variant="h6" sx={headerStyles} >{year}</Typography>
+      <Typography variant="body1" sx={{ color: theme.palette.secondary.main }}>ft. Performances By</Typography>
+      <List sx={{ padding: 0 }}>
+        {performingEnsemblesToDisplay}
+      </List>
+    </Box>
   )
 }
 
