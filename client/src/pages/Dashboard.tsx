6ee9baf1217b82c9834b30323c0234d6df6e141e
andrew <andrewasmit@hotmail.com>
2023-11-17 09:15:58 -0600

    feat: incorporate MUI charts and updated scroll overflow in mainContainer

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,6 +1,7 @@
 // External Depencies
 import { useEffect, useMemo } from "react";
 import { Box, Typography, useTheme } from "@mui/material";
+import { PieChart } from '@mui/x-charts/PieChart';
 
 // Internal Depencies
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
@@ -12,6 +13,7 @@ import { useFetchOrganizationData } from "../hooks/api/useFetchOrganizationData"
 import "../App.css";
 import logo from '/logo-color.svg'
 import Loading from "../components/shared/Loading";
+import { BarChart } from "@mui/x-charts";
 
 
 function Dashboard() {
@@ -56,6 +58,8 @@ function Dashboard() {
     return <Loading />
   }
 
+  console.log(fetchedOrganizationData);
+
   return (
     <div>
       <Box sx={{ background: theme.palette.secondary.main }} >
@@ -74,13 +78,46 @@ function Dashboard() {
         />
       </Box>
 
-      <Typography variant="h1">Repertoire</Typography>
+      <Typography variant="h1" sx={{ fontSize: 80 }}>
+        {fetchedOrganizationData?.name}
+      </Typography>
 
       <Typography variant="h3">This is the Dashboard</Typography>
 
       <Typography variant="body1">
         This is where all of your organization information will be
       </Typography>
+
+      <PieChart
+        series={[
+          {
+            data: [
+              { id: 0, value: 10, label: 'series A' },
+              { id: 1, value: 15, label: 'series B' },
+              { id: 2, value: 20, label: 'series C' },
+            ],
+          },
+        ]}
+        width={400}
+        height={200}
+      />
+
+      <BarChart
+        xAxis={[
+          {
+            id: 'barCategories',
+            data: ['bar A', 'bar B', 'bar C'],
+            scaleType: 'band',
+          },
+        ]}
+        series={[
+          {
+            data: [2, 5, 3],
+          },
+        ]}
+        width={500}
+        height={300}
+      />
     </div>
   );
 }
